934f2d7b4d375b69a4cc1e5b36a637057505a546
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-04-28 18:04:02 -0700

    Add webpack test package

---
+++ webpack.config-test.js
@@ -0,0 +1,18 @@
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+};
